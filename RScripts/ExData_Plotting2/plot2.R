cfb714a680d815a13724538d00ef276dcb5811cf
lukeyanchen <lukeyanchen@gmail.com>
2016-09-30 17:45:43 -0700

    Create plot2.R

---
+++ RScripts/ExData_Plotting2/plot2.R
@@ -0,0 +1,26 @@
+### Download file if does not exist ###
+filename <- "exdata%2Fdata%2FNEI_data.zip"
+
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+  download.file(fileURL, filename, method="curl")
+}
+
+### Unzip files if they do not exist ###
+if (!file.exists("summarySCC_PM25.rds")) { 
+  unzip(filename) 
+}
+
+### Read files ###
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+### Sum up emissions by year ###
+baltimore <- NEI[which(NEI$fips=="24510"),]
+totalyearlyemissions <- aggregate(Emissions~year, data=baltimore, FUN="sum")
+
+### Plot graph ###
+png("plot2.png")
+options("scipen" = 20)
+barplot(height=totalyearlyemissions$Emissions,names.arg=totalyearlyemissions$year,main="Total Emissions from 1999 to 2008 in Baltimore",ylab="Amount of PM2.5 emitted, in tons",xlab="Year")
+dev.off()
