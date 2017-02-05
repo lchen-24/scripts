567e81df4b954c4c361692f25e9994268eac0b5b
lukeyanchen <lukeyanchen@gmail.com>
2017-02-04 17:42:07 -0800

    new files

---
+++ PythonScripts/test2.py
@@ -0,0 +1,10 @@
+fname = raw_input('Enter file name: ')
+if ( len(fname) < 1 ) : fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: ') : continue
+    pieces = line.split()
+    email = pieces[1]
+    print email
+    org = email.split("@",1)[1]
+    print org
