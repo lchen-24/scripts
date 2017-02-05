ab066332b309831768136bea70ef946ca561e810
lukeyanchen <lukeyanchen@gmail.com>
2017-02-04 18:01:12 -0800

    add new files

--- PythonScripts/test2.py
+++ PythonScripts/test2.py
@@ -1,10 +1,34 @@
+import xml.etree.ElementTree as ET
+
 fname = raw_input('Enter file name: ')
-if ( len(fname) < 1 ) : fname = 'mbox.txt'
-fh = open(fname)
-for line in fh:
-    if not line.startswith('From: ') : continue
-    pieces = line.split()
-    email = pieces[1]
-    print email
-    org = email.split("@",1)[1]
-    print org
+if ( len(fname) < 1 ) : fname = 'Library.xml'
+
+# <key>Track ID</key><integer>369</integer>
+# <key>Name</key><string>Another One Bites The Dust</string>
+# <key>Artist</key><string>Queen</string>
+def lookup(d, key):
+    found = False
+    for child in d:
+        if found : return child.text
+        if child.tag == 'key' and child.text == key :
+            found = True
+    return None
+
+stuff = ET.parse(fname)
+all = stuff.findall('dict/dict/dict')
+print 'Dict count:', len(all)
+for entry in all:
+    if ( lookup(entry, 'Track ID') is None ) : continue
+
+    name = lookup(entry, 'Name')
+    artist = lookup(entry, 'Artist')
+    album = lookup(entry, 'Album')
+    count = lookup(entry, 'Play Count')
+    rating = lookup(entry, 'Rating')
+    length = lookup(entry, 'Total Time')
+    genre = lookup(entry, 'Genre')
+
+    if name is None or artist is None or album is None : 
+        continue
+
+    print name, artist, album, count, rating, length, genre
